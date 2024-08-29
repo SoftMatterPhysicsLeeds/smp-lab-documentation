e20211f5fb5d5986b2a910fe56c611fe9b0da714
danlindleybaker <danlindleybaker@gmail.com>
2024-08-29 09:34:26 +0100

    Deployed 0df1bca with MkDocs version: 1.6.0

---
+++ javascripts/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true,
+      equationNumbers: {autoNumber: "AMS"},
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  document$.subscribe(() => { 
+  
+  
+    MathJax.typesetPromise()
+  })
